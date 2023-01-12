98d34f06996a5c30bcc2dbbd2de9f1f4c4255ad3
raziyeh masumshah <r_masumshah@sbu.ac.ir>
2023-01-12 15:23:51 +0330

    Create Chapter1.py

---
+++ Chapter1/Chapter1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #                              Installation
+
+# Environment:
+# 
+# * step1: conda create --name myenv
+# * step2: conda activate myenv
+# 
+# Python:
+# 
+# * conda install python=3.6
+# 
+# Packages:
+# 
+# * conda install pip
+# * conda install -c conda-forge matplotlib==3.3.4
+# * conda install -c r rpy2
+# * pip3 install numpy==1.18.1
+# * pip3 install pandas==1.0.1
+# * pip3 install scikit-learn==0.21.2
+# * pip3 install seaborn==0.11.2
+# * pip3 install wget
+# * pip3 install biopython==1.76
